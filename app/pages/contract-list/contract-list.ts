fcdfff8f3470bacefc0c0c6b1ca6fa614d915247
chatkup K <chatkup@gmail.com>
2016-09-24 23:17:38 +0700

    run OK, rest: login, undefine lnmain lnsubmain

--- app/pages/contract-list/contract-list.ts
+++ app/pages/contract-list/contract-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 /*
   Generated class for the ContractListPage page.
@@ -11,9 +11,45 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/contract-list/contract-list.html',
 })
 export class ContractListPage {
+  main: any[]
+  submain: any[]
+  allsubmain: any[]
+  contract: any[]
+  lnmain: number
+  lnsubmain: number
 
   constructor(private navCtrl: NavController) {
-
+    // ให้เลือกว่าจะลง chem หรือ CBC หรือ DTX
+    this.main = [
+      { id: 1, name: 'สัญญา Chemistry' },
+      { id: 2, name: 'สัญญา CBC' },
+      { id: 3, name: 'สัญญา แถบตรวจน้ำตาลปลายนิ้ว' },
+    ]
+    // ต่อไปต้องดึงสัญญามาตามคนที่ login ว่าอยู่ รพ.ไหน ระดับ S/M/L/XL 
+    this.allsubmain = [
+      { id: 1, mainId: 1, name: 'S' },
+      { id: 2, mainId: 1, name: 'M' },
+      { id: 3, mainId: 1, name: 'L' },
+      { id: 4, mainId: 1, name: 'XL' },
+      { id: 5, mainId: 2, name: 'S' },
+      { id: 6, mainId: 2, name: 'M' },
+      { id: 7, mainId: 2, name: 'L' },
+      { id: 8, mainId: 2, name: 'XL' },
+    ]
   }
 
+  getsubmain() {
+    this.submain = [];
+    let lnmain = this.lnmain;
+    this.allsubmain.forEach(v => {
+      if (v.mainId == lnmain) {
+        this.submain.push(v)
+      }
+    })
+  }
+
+  showselected() {
+    console.log(this.lnmain)
+    console.log(this.lnsubmain)
+  }
 }
